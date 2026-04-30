edc7b9a10f45020ce5aecb5fec2ba1fb2391e23c
Theophillus-Keitumetse <mk24019719@biust.ac.bw>
2026-04-30 23:30:09 +0200

    fix navbar css import case sensitivity for railway

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { FiSearch, FiBell } from "react-icons/fi";
-import "./Navbar.css";
+import "./navbar.css";
 import { useEffect, useState } from "react";
 
 export default function Navbar() {
