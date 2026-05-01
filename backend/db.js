7894fcce5bd35983f35d0c41d477a5f6e0c15d70
Theophillus-Keitumetse <mk24019719@biust.ac.bw>
2026-05-02 00:00:05 +0200

    Fix DB connection handling using DATABASE_URL

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,18 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const mysql = require('mysql2/promise');
+const { URL } = require('url');
+require('dotenv').config();
 
-const db = mysql.createPool(process.env.DATABASE_URL);
+// Parse the DATABASE_URL string
+const dbUrl = new URL(process.env.DATABASE_URL);
+
+const db = mysql.createPool({
+    host: dbUrl.hostname,
+    user: dbUrl.username,
+    password: dbUrl.password,
+    database: dbUrl.pathname.split('/')[1],
+    port: dbUrl.port,
+    waitForConnections: true,
+    connectionLimit: 10,
+});
 
 module.exports = db;
